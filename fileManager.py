01058f53dac1209c5f24db8d3a60439deadbf9f3
nicoquagliata <nicoquagliata@gmail.com>
2019-08-30 12:40:34 -0300

    intro to reg exp

--- fileManager.py
+++ fileManager.py
@@ -1,1 +1,12 @@
 #file manager chapter
+import re
+archivo = open("sample.txt", encoding="utf-8")
+informacion = archivo.read()
+archivo.close()
+
+print(informacion)
+
+print(re.match(r"abcdefghijklmnopqrstuvwxyz",informacion))
+print(re.match(r"abcdefghijk4qrstuvwxyz",informacion))
+
+print(re.search(r"0123456789",informacion))
